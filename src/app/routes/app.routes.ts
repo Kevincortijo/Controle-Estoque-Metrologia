09ae57018a15fa15be0f4931190406587e37c3fb
Joel Junior <joelrool.junior@lgepartner.com>
2025-06-30 15:48:42 -0400

    :sparkles: feat:routs to home page

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from '../home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Home
+    },
+    {
+        path:'home',
+        component: Home
+    }
+];
