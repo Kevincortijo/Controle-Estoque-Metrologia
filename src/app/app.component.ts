fa6bca51254e1d83cba2fdec4b0d4f9a26f22f47
Andrew Cortijo <andrew1.cortijo@lge.com>
2025-07-09 16:40:01 -0400

    Register component modification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Sidebar, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class App {
-  protected title = 'Controle-Estoque-Metrologia';
+  title = 'Controle-Estoque-Metrologia';
+  hideSidebar = false;
+  sidebarOpen = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const hideSidebarRoutes = ['/', '/login'];
+      this.hideSidebar = hideSidebarRoutes.includes(event.urlAfterRedirects);
+
+      if (this.hideSidebar) {
+        this.sidebarOpen = false;
+      } else {
+        this.sidebarOpen = true; 
+      }
+    });
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
