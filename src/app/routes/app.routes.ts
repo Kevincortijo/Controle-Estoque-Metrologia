1f6748d975e3650a20ba7a4d110d68358b6baa24
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-07-14 11:04:32 -0400

    :recycle: refactor: preparing to delete the sidebar component

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,43 +1,23 @@
 import { Routes } from '@angular/router';
-import { Home } from '../pages/home/home.component'
+import { Home } from '../pages/home/home.component';
 import { Sidebar } from '../components/sidebar/sidebar.component';
 import { Login } from '../pages/login/login.component';
 import { Moving } from '../pages/moving/moving.component';
 import { Overview } from '../pages/overview/overview.component';
 import { Register } from '../pages/register/register.component';
 import { Table } from '../components/table/table.component';
+import { Sidenav } from '../components/sidenav/sidenav.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:Login
-    },
-    {
-        path:'login',
-        component:Login
-    },
-    {
-        path:'home',
-        component: Home
-    },
-    {
-        path:'moving',
-        component:Moving
-    },
-    {
-        path:'overview',
-        component:Overview
-    },
-    {
-        path:'register',
-        component:Register
-    },
-    {
-        path: 'sidebar-preview',
-        component:Sidebar
-    },
-    {
-        path: 'table-preview',
-        component: Table
-    }
+    {path:'login', component:Login},
+    {path:'home', component: Home},
+    {path:'moving', component: Moving},
+    {path:'overview', component: Overview},
+    {path:'register', component: Register},
+    
+    {path: 'sidebar-preview', component: Sidebar},
+    {path: 'sidenav-preview', component: Sidenav},
+    {path: 'table-preview', component: Table},
+    
+    {path:'',component:Login},
 ];
