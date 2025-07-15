219e0152f1ab979eb45b24318f61995421baf53d
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-07-15 11:36:17 -0400

    :sparkles: feat: add global variables to css and html for home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Sidenav } from "./components/sidenav/sidenav.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Sidenav],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
