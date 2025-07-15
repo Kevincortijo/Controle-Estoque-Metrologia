7a03e85ee448f813cdacd1c69794f2b1c4a06886
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-07-15 11:36:49 -0400

    feta

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidenavContent } from "../sidenav-content/sidenav-content.component";
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [MatSidenavModule, SidenavContent],
+  imports: [
+    MatSidenavModule,
+    SidenavContent,
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class Sidenav {
-
+  constructor(public router: Router){}
+  
+  isloginpage(): boolean {
+    return this.router.url === '/login';
+  }
 }
