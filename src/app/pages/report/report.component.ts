401a5c0936600831bfa28ac235b302dae3ee75a2
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-07-16 01:41:45 -0400

    :sparkles: feat: added content to report component

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-report.component',
-  imports: [],
+  imports: [
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
 export class Report {
+ selectedFormat: string = '';
 
+  formats = [
+    { label: 'PDF', value: 'pdf' },
+    { label: 'Excel (.xlsx)', value: 'xlsx' },
+    { label: 'CSV', value: 'csv' },
+    { label: 'JSON', value: 'json' }
+  ];
+
+  export() {
+    // Apenas simula exportação
+    alert(`Exportando filtros como ${this.selectedFormat.toUpperCase()}...`);
+  }
 }
