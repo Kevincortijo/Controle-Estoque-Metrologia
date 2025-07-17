f4d67edc9a8a41e02152e885ec77f247be266569
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-07-16 23:31:12 -0400

    :sparkles: feat: added content to solicitation component

--- src/app/pages/solicitation/solicitation.component.ts
+++ src/app/pages/solicitation/solicitation.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-solicitation.component',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './solicitation.component.html',
   styleUrl: './solicitation.component.scss'
 })
--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -6,6 +6,7 @@ import { Overview } from '../pages/overview/overview.component';
 import { Register } from '../pages/register/register.component';
 import { Table } from '../components/table/table.component';
 import { Sidenav } from '../components/sidenav/sidenav.component';
+import { Solicitation } from '../pages/solicitation/solicitation.component';
 
 export const routes: Routes = [
     {path:'login', component:Login},
@@ -13,6 +14,7 @@ export const routes: Routes = [
     {path:'moving', component: Moving},
     {path:'overview', component: Overview},
     {path:'register', component: Register},
+    {path:'solicitation', component: Solicitation},
     
     {path: 'sidenav-preview', component: Sidenav},
     {path: 'table-preview', component: Table},
