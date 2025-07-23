b2a48697bdeec3da79dc55770733d840e7b0a52b
Andrew Cortijo <andrew1.cortijo@lge.com>
2025-07-23 15:40:07 -0400

    Overview modification

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-overview',
   imports: [
     MatCardModule,
     MatIconModule,
+    RouterModule
   ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
