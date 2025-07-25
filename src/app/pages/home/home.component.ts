a5a93a8e3740c8a2794e2f6f026573ecd9ca0175
Joel Junior <joelrool.junior@lgepartner.com>
2025-07-25 16:26:35 -0400

    profile user icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Table } from "../../components/table/table.component";
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
