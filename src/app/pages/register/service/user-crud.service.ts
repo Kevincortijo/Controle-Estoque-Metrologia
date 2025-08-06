1ab52978b50aca1c7e51725e23fc72ded1820884
JoelRoolJunior <141271415+JoelRoolJunior@users.noreply.github.com>
2025-08-06 00:51:13 -0400

    :sparkles: feat: added abstract class to User crud

--- src/app/pages/register/service/user-crud.service.ts
+++ src/app/pages/register/service/user-crud.service.ts
@@ -1,15 +1,42 @@
-import { Signal } from "@angular/core";
+import { Injectable, signal, computed } from "@angular/core";
+import { AbstractUserService } from "./user-crud-abstract.service";
 import { User } from "../../../model/user.model";
-import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { Observable, of } from "rxjs";
 import { OperationResult } from "../../../model/operation-result.model";
 
-export abstract class AbstractUserService{
-    abstract users: Signal<User[]>;
-    abstract refresh(): void;
-    abstract add(user: Omit<User, 'id'>): Observable<OperationResult>;
-    abstract update(user: User): Observable<OperationResult>;
-    abstract remove(id: number): Observable<OperationResult>;
-    abstract search(query: string): Observable<OperationResult>;
-    abstract search_by_id(id: number): Observable<OperationResult>;
-     
+@Injectable()
+export class UserService extends AbstractUserService{
+    private _users = signal<User[]>([]);
+
+    users = computed(()=> this._users());
+
+    constructor(private http: HttpClient){
+        super();
+        this.refresh();
+    }
+
+    override refresh(): void {
+        
+    }
+
+    override add(user: Omit<User, "id">): Observable<OperationResult> {
+        return of()
+    }
+
+    override remove(id: number): Observable<OperationResult> {
+        return of()
+    }
+
+    override update(user: User): Observable<OperationResult> {
+        return of()
+    }
+
+    override search(query: string): Observable<OperationResult> {
+        return of()
+    }
+
+    override search_by_id(id: number): Observable<OperationResult> {
+        return of()
+    }
 }
